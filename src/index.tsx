dafa5a5ef0111cf3e297d6fa1ef15cd3d0f9d62e
romanmunar <roman.munar1@gmail.com>
2021-01-22 18:15:52 +0800

    added redux toolkit

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
-import React from "react"
 import ReactDOM from "react-dom"
-import { App } from "./App"
 import reportWebVitals from "./reportWebVitals"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-)
+const render = () => {
+  const App = require("./App").default
+
+  ReactDOM.render(<App />, document.getElementById("root"))
+}
+
+render()
+
+if (process.env.NODE_ENV === "development" && module.hot) {
+  module.hot.accept("./App", render)
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
