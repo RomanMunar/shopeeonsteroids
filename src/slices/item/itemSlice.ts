9a01646113717ff2ab445c6e30550e981bff4dd6
romanmunar <roman.munar1@gmail.com>
2021-01-24 21:56:54 +0800

    migrated itemSlice to createAsyncThink

--- src/slices/item/itemSlice.ts
+++ src/slices/item/itemSlice.ts
@@ -1,54 +1,59 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit"
-import { AppThunk } from "src/App/store"
-import { getItem } from "src/lib/api/searchAPI"
-import { ItemDetailed } from "src/lib/types"
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { getItem } from "src/lib/api";
+import { ItemDetailed } from "src/lib/types";
 
 interface ItemState {
-  item: ItemDetailed | null
-  loading: boolean
-  error: string | null
+  item: ItemDetailed | null;
+  fetchStatus: "fulfilled" | "pending" | "idle";
+  error: string | null | boolean;
 }
 
-interface ItemLoaded {
-  item: ItemDetailed
-}
+export const fetchDisplayDetailedItem = createAsyncThunk<
+  ItemDetailed,
+  { itemid: number; shopid: number },
+  { rejectValue: string }
+>("selectedItems/fetchDetailedById", async (data, { rejectWithValue, dispatch }) => {
+  const { itemid, shopid } = data;
 
+  dispatch(itemStart());
+
+  try {
+    return await getItem({ itemid, shopid });
+  } catch (err) {
+    return rejectWithValue("Error");
+  }
+});
 const initialState: ItemState = {
   item: null,
-  loading: false,
+  fetchStatus: "idle",
   error: null,
-}
+};
 
 const itemShopee = createSlice({
   name: "item",
   initialState,
   reducers: {
     itemStart(state) {
-      state.loading = true
-      state.error = null
-    },
-    itemSuccess(state, action: PayloadAction<ItemLoaded>) {
-      const { item } = action.payload
-      state.item = item
-      state.loading = false
-      state.error = null
-    },
-    itemFailure(state, action: PayloadAction<string>) {
-      state.loading = false
-      state.error = action.payload
+      state.fetchStatus = "idle";
     },
   },
-})
+  extraReducers: (builder) => {
+    builder.addCase(fetchDisplayDetailedItem.fulfilled, (state, { payload: item }) => {
+      //remove past
+      state.item = item;
+      state.fetchStatus = "fulfilled";
+      state.error = null;
+    });
+    builder.addCase(fetchDisplayDetailedItem.rejected, (state, { payload }) => {
+      state.fetchStatus = "fulfilled";
+      if (payload) {
+        state.error = payload;
+      } else {
+        state.error = true;
+      }
+    });
+  },
+});
 
-export const { itemStart, itemSuccess, itemFailure } = itemShopee.actions
-export default itemShopee.reducer
-
-export const item = ({ itemid, shopid }: { itemid: number; shopid: number }): AppThunk => async (dispatch) => {
-  try {
-    dispatch(itemStart())
-    const item = await getItem({ itemid, shopid })
-    dispatch(itemSuccess({ item }))
-  } catch (err) {
-    dispatch(itemFailure(err))
-  }
-}
+export const { itemStart } = itemShopee.actions;
+export default itemShopee.reducer;
