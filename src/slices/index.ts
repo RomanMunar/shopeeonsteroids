66bec2b092613615302f7a0c8899de3b0f333c39
romanmunar <roman.munar1@gmail.com>
2021-01-27 15:20:53 +0800

    fixed selected Items reducer

--- src/slices/index.ts
+++ src/slices/index.ts
@@ -1,4 +1,8 @@
 export { default as selectedItemsReducer } from "./selectedItems/selectedItemsSlice";
+export type { SelectedItem } from "./selectedItems/selectedItemsSlice";
 export { default as searchReducer } from "./search/searchSlice";
-export { default as ItemsReducer } from "./item/itemSlice";
+export { default as ItemReducer } from "./item/itemSlice";
 export { default as shopReducer } from "./shop/shopSlice";
+export { default as UIReducer } from "./ui/UISlice";
+
+export type CompareLayout = "double" | "triple" | "all";
--- src/slices/selectedItems/selectedItemsSlice.ts
+++ src/slices/selectedItems/selectedItemsSlice.ts
@@ -1,20 +1,25 @@
 import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "src/App/rootReducer";
 import { getItem } from "src/lib/api";
-import { Item, ItemDetailed } from "src/lib/types";
+import { ItemDetailed, SearchItem } from "src/lib/types";
 
-interface SelectedItem {
-  item: Item | ItemDetailed;
+export interface SelectedItem extends SearchItem {
+  error: ErrorValues | null | boolean;
+  fetchStatus: "fulfilled" | "pending" | "idle";
+}
+export interface SelectedItemDetailed extends ItemDetailed {
   error: ErrorValues | null | boolean;
   fetchStatus: "fulfilled" | "pending" | "idle";
 }
 
 interface SelectedItemsState {
-  selectedItems: SelectedItem[];
+  selectedItems: (SelectedItem | SelectedItemDetailed)[];
+  isEmpty: boolean;
 }
 
 const initialState: SelectedItemsState = {
   selectedItems: [],
+  isEmpty: true,
 };
 
 interface ErrorValues {
@@ -46,7 +51,7 @@ export const fetchDetailedItem = createAsyncThunk<
   {
     condition: ({ itemid }, { getState }) => {
       const { selectedItems } = getState().selectedItemsReducer;
-      const item = selectedItems.find((i) => i.item.itemid === itemid);
+      const item = selectedItems.find((i) => i.itemid === itemid);
       if (item?.fetchStatus == "fulfilled" || item?.fetchStatus == "pending") {
         // Already fetched or in progress, don't need to re-fetch
         return false;
@@ -59,18 +64,22 @@ export const selectedItemsShopee = createSlice({
   name: "selectedItems",
   initialState,
   reducers: {
-    selectItem(state, action: PayloadAction<{ item: Item }>) {
+    selectItem(state, action: PayloadAction<{ item: SearchItem }>) {
+      if (state.isEmpty) state.isEmpty = false;
+
       const { item } = action.payload;
-      const newItem: SelectedItem = { item, fetchStatus: "idle", error: null };
+      const newItem: SelectedItem = { ...item, fetchStatus: "idle", error: null };
 
       state.selectedItems.push(newItem);
     },
-    unselectItem(state, action: PayloadAction<{ selectedItem: SelectedItem }>) {
+    unselectItem(state, action: PayloadAction<{ selectedItem: SelectedItem | SearchItem }>) {
+      if (state.selectedItems.length <= 1) state.isEmpty = true;
+
       const { selectedItem } = action.payload;
-      state.selectedItems.filter((i) => selectedItem.item.itemid === i.item.itemid);
+      state.selectedItems = state.selectedItems.filter((i) => selectedItem.itemid !== i.itemid);
     },
     selectedItemStart(state, action: PayloadAction<{ itemid: number }>) {
-      const item = state.selectedItems.find((i) => i.item.itemid === action.payload.itemid);
+      const item = state.selectedItems.find((i) => i.itemid === action.payload.itemid);
       if (!item) return;
       item.fetchStatus = "pending";
     },
@@ -78,12 +87,12 @@ export const selectedItemsShopee = createSlice({
   extraReducers: (builder) => {
     builder.addCase(fetchDetailedItem.fulfilled, (state, { payload: item }) => {
       //remove past
-      state.selectedItems.filter((i) => i.item.itemid === item.itemid);
-      const newItem = { item, fetchStatus: "idle", error: null } as SelectedItem;
+      state.selectedItems.filter((i) => i.itemid === item.itemid);
+      const newItem = { ...item, fetchStatus: "idle", error: null } as SelectedItemDetailed;
       state.selectedItems.push(newItem);
     });
     builder.addCase(fetchDetailedItem.rejected, (state, { payload }) => {
-      const item = state.selectedItems.find((i) => i.item.itemid !== payload?.itemid);
+      const item = state.selectedItems.find((i) => i.itemid !== payload?.itemid);
       if (!item) {
         return;
       }
