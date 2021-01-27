d247fe64803aaea60449afa602a61bac2d08a00a
romanmunar <roman.munar1@gmail.com>
2021-01-27 15:18:37 +0800

    added selectedItem card and skeleton component

---
+++ src/components/product/selectedItemCard.tsx
@@ -0,0 +1,11 @@
+import {Box} from "@chakra-ui/react"
+
+const selectedItemCard = () => {
+  return (
+    <Box rounded="md" h="130px" w="100%">
+      Hey
+    </Box>
+  );
+};
+
+export default selectedItemCard;
---
+++ src/components/product/selectedItemSkeleton.tsx
@@ -0,0 +1,7 @@
+import { Skeleton } from "@chakra-ui/react";
+
+const selectedItemSkeleton = () => {
+  return <Skeleton rounded="md" h="130px" w="100%" />;
+};
+
+export default selectedItemSkeleton;
