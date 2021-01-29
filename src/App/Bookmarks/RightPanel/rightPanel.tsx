d46b255ad29e857e7f1b32e033188cddc594f5d8
romanmunar <roman.munar1@gmail.com>
2021-01-29 18:48:14 +0800

    added bookmarks' rightPanel

--- src/App/Bookmarks/RightPanel/rightPanel.tsx
+++ src/App/Bookmarks/RightPanel/rightPanel.tsx
@@ -1,11 +1,15 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Heading, Text } from "@chakra-ui/react";
 
 const rightPanel = () => {
   return (
-    <Box overflowY="auto" h="100vh" w="220px" borderColor="blue.300" flex="none">
-      <Heading as="h3" size="sm" p={4} alignSelf="start">
-        FILTER
+    <Box overflowY="auto" h="100vh" p={4} pt="10" flex="1" borderColor="blue.300">
+      <Heading as="h3" size="lg" alignSelf="start">
+        Replacement for hazelnut coffee
       </Heading>
+      <Text my="4">
+        Coffee Grounds I&apos;ll test in the future, getting pretty used to the taste of hazelnut
+        want something new
+      </Text>
     </Box>
   );
 };
