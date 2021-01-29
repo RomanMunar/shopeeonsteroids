7575fec547ad6bbede66f0c74dce7d660503d09a
romanmunar <roman.munar1@gmail.com>
2021-01-29 18:49:06 +0800

    added mockDatas

---
+++ src/App/Search/mockRatings.ts
@@ -0,0 +1,176 @@
+export const mockRatings = {
+  data: [
+    {
+      anonymous: false,
+      author_portrait: "",
+      author_username: "mariefernandez28",
+      comment:
+        "Sarap na sarap mga alaga ko pati yung lata kinuha pa para simutin grabe din sa balot haha okay lang naman kahit hindi na balutin mabuti next time po ulit salamat",
+      images: [
+        "9a56bc4e891acdd7e67d474c051fd68d",
+        "740f4cf51b4a4650c2e99ee0c173cef3",
+        "0e92c915f486b43542cfdc3c0841ea56",
+        "18bc9e3bd38ac273c613c1083c431b29",
+        "c358ae91c1b4ecf8eef91b69e5f68f00",
+      ],
+      rating_star: 5,
+      product_items: [{ model_name: "Lamb" }, { model_name: "Chiken & Lamb" }],
+      tags: [
+        { tag_description: "Excellent quality ", tag_id: 51 },
+        { tag_description: "Very accommodating seller ", tag_id: 52 },
+        { tag_description: "Well-packaged ", tag_id: 53 },
+        { tag_description: "Item shipped immediately ", tag_id: 54 },
+        { tag_description: "Will order again ", tag_id: 55 },
+      ],
+    },
+    {
+      anonymous: false,
+      author_portrait: "",
+      author_username: "chippy_atom",
+      comment:
+        "This is just a repeat purchase from my favorite pet shop place. The only place I could get the cheapest pet essentials. This product is slso one of my dogs 'favorites. Thank you seller and shopee. Till next transactions. ",
+      images: [
+        "f708f5b469b53391ae0a3bc8ca1bea0b",
+        "110e65c32565200e5b89b4346c81bf66",
+        "195e3e0f5954dba913c192e94dc351ec",
+        "3283678ed74a5798bfe5dacdcdf95dd0",
+        "8294403db9797b76c88c74947790b573",
+      ],
+      rating_star: 5,
+      product_items: [
+        { model_name: "Lamb" },
+        { model_name: "Chicken" },
+        { model_name: "Chiken & Lamb" },
+      ],
+      tags: [
+        { tag_description: "Excellent quality ", tag_id: 51 },
+        { tag_description: "Very accommodating seller ", tag_id: 52 },
+        { tag_description: "Well-packaged ", tag_id: 53 },
+        { tag_description: "Item shipped immediately ", tag_id: 54 },
+        { tag_description: "Will order again ", tag_id: 55 },
+      ],
+    },
+    {
+      anonymous: true,
+      author_portrait: "bb9a8330945e6351632430928be0195a",
+      author_username: "m*****a",
+      comment:
+        "2nd time to order from this shop... always excellent service!\n\nOrdered 20 cans and all of them, 2023 pa expiration date, walang yupi and well-packaged. Naka-box and bubble wrap outside the box. \n\nMy dogs especially love the chicken flavor 💖",
+      images: [
+        "b690e1b000c373b741f729d06f9b1059",
+        "8a6fd52140857f29c0a54aaa87366827",
+        "a0eac6b548e58dc779b853dd04271e79",
+      ],
+      rating_star: 5,
+      product_items: [
+        { model_name: "Lamb" },
+        { model_name: "Chicken" },
+        { model_name: "Chiken & Lamb" },
+      ],
+      tags: [
+        { tag_description: "Well-packaged ", tag_id: 53 },
+        { tag_description: "Item shipped immediately ", tag_id: 54 },
+        { tag_description: "Will order again ", tag_id: 55 },
+      ],
+    },
+    {
+      anonymous: true,
+      author_portrait: "",
+      author_username: "s*****a",
+      comment:
+        'Received complete items. Three cans of bark may dents po, yung isa malaki at malalim kaya di ko na napakain sa aso ko. Yung iba ok naman po. Sa courier sana ingatan nyo ang parcel. Sana po mas sturdy na box po gamitin nyo next time. Suggestion po to put "fragile" po sa box. Salamat po.',
+      images: ["250ab9ee6ca9eb466aa55fe26598aae7", "7b2fa363d29a1c4c37a56e3a52f35ad1"],
+      rating_star: 3,
+      product_items: [{ model_name: "Lamb" }, { model_name: "Lamb & Rice" }],
+      tags: null,
+    },
+    {
+      anonymous: false,
+      author_portrait: "78965b81d3d78a7b31fbb98fb89b5e69",
+      author_username: "ymayyalabador",
+      comment:
+        "Poor customer service and poor comprehension nung seller lol, ok naman lahat ng items kaso etong seller ang sungit and hindi professional sumagot hindi makokomplete tong order ko kung hindi kopa siya niremind na kulanng yung nasa picture na sinend niya",
+      images: [
+        "a9781effc2e011eb25a4d7898af113ab",
+        "12a6ba416f31f02905a13831808d3187",
+        "87049c55665bd3bc68308ba4cdb2f0a3",
+      ],
+      rating_star: 2,
+      product_items: [{ model_name: "Chiken & Lamb" }],
+      tags: null,
+    },
+    {
+      anonymous: true,
+      author_portrait: "",
+      author_username: "s*****a",
+      comment:
+        " I received the items in good condition. Good thing na this time walang dents sa canned dog foods unlike last time. Sana lagi ganito. Kaya lang di naka bubble wrap yung 2 doggymin at nakaipit sa gilid kaya yung box deformed na pero buti di nagleak. Naka bubble wrap naman yung LC vit. Salamat po.",
+      images: ["8975e3d5b81197c825952fdb764bf2f2"],
+      rating_star: 5,
+      product_items: [
+        { model_name: "Lamb" },
+        { model_name: "Chiken & Lamb" },
+        { model_name: "Lamb & Rice" },
+      ],
+      tags: null,
+    },
+    {
+      anonymous: false,
+      author_portrait: "498e3a76ccc1bf8f268b2aa9d91d530f",
+      author_username: "aronimous",
+      comment:
+        'Solid super thanks seller@#££____(&(&(&&(&(&:;+228£+_;_;_\'+_8£(#£!"+_£8£8£_!";382(£";";£+38"+"";#(#£9";"+##8"+£;"+£9#+_-4+3_8\'8&8&8&&(&8&(&838£(£+__!_+_+_+__(£(39££(£(_-47£\'8_;2(#8_-42+#(*8£9#!"+£92£8_+£(£8*(£(#;#*(#9#;£+£(#9£+";"(£9#£("+£(£(£(£(29#(£(££(£(39_+_+_(393£(£(£(3(__(4+_+829192377_\'7:+#;#',
+      images: ["0786e32c3a7497fd801bfadb3ff34edc"],
+      rating_star: 5,
+      product_items: [{ model_name: "Lamb" }],
+      tags: [
+        { tag_description: "Excellent quality ", tag_id: 51 },
+        { tag_description: "Very accommodating seller ", tag_id: 52 },
+        { tag_description: "Well-packaged ", tag_id: 53 },
+        { tag_description: "Item shipped immediately ", tag_id: 54 },
+        { tag_description: "Will order again ", tag_id: 55 },
+      ],
+    },
+    {
+      anonymous: false,
+      author_portrait: "296e7cbd2aba0de2dc2c6f337a0907ae",
+      author_username: "jasminangelatri",
+      comment:
+        "Ordered 1/21. Delivered 1/24/21. All items complete &properly packed, always reliable seller with reasonably, if not cheaper priced pet foods. My dogs love these. Good quality,  cheaper wet dog food.",
+      images: ["d67466695a86cb222fb6f5fa879722b2", "90e2afa7d7eb5880a914880fc8da004c"],
+      rating_star: 5,
+      product_items: [{ model_name: "Chiken & Lamb" }],
+      tags: [
+        { tag_description: "Excellent quality ", tag_id: 51 },
+        { tag_description: "Very accommodating seller ", tag_id: 52 },
+        { tag_description: "Well-packaged ", tag_id: 53 },
+        { tag_description: "Item shipped immediately ", tag_id: 54 },
+        { tag_description: "Will order again ", tag_id: 55 },
+      ],
+    },
+    {
+      anonymous: false,
+      author_portrait: "",
+      author_username: "carinchew",
+      comment:
+        "I bought different brands of dog/cat food and requested them to label each. Unfortunately, they only wrote `dog food' while other packs have no label at all. Not even an expiration date. They sent incomplete orders, I contacted them and they offered a refund. Overall, a bad experience. ",
+      images: ["1a309423c7e307a484b6216ca281a91c"],
+      rating_star: 3,
+      product_items: [{ model_name: "Lamb" }],
+      tags: null,
+    },
+    {
+      anonymous: true,
+      author_portrait: "0d2ca3631688849f940db6e8b6c7cd72",
+      author_username: "a*****o",
+      comment:
+        "Dito ako lagi nag order ng mga itemem ng babies ko\nMura at mabilis ang delivery\nPag may problem namam mabilis sila sumagot at mag respond\nDouble check lang lagi para maiwasan ang may maiwan na item\n\nPero dito padin ako mag order\nLegit seller",
+      images: ["58b89579d0f7243ce2c72f60426a572a"],
+      rating_star: 5,
+      product_items: [{ model_name: "Chicken" }, { model_name: "Chiken & Lamb" }],
+      tags: null,
+    },
+  ],
+  error: 0,
+  error_msg: null,
+};
