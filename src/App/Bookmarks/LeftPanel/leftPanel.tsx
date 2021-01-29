7bce2480b66a408768093c79a59dd6fb12745812
romanmunar <roman.munar1@gmail.com>
2021-01-29 18:48:40 +0800

    improved spacing

--- src/App/Bookmarks/LeftPanel/leftPanel.tsx
+++ src/App/Bookmarks/LeftPanel/leftPanel.tsx
@@ -24,7 +24,7 @@ const leftPanel = () => {
       <Heading as="h3" size="sm" px="2" py="4" alignSelf="start">
         BOOKMARKS
       </Heading>
-      <Box px="2">
+      <Box my="4" px="2">
         <InputGroup>
           <InputLeftElement pointerEvents="none">
             <Search width="20" height="20" />
@@ -32,13 +32,14 @@ const leftPanel = () => {
           <Input type="text" placeholder="Coffee..." />
         </InputGroup>
       </Box>
-      <Box bg="gray.50" display="flex" alignItems="start" flex="1" my="2" shadow="inner">
+      <Box bg="gray.50" display="flex" alignItems="start" flex="1" shadow="inner">
         <VStack
           borderY="1px"
-          borderColor="gray.300"
+          borderColor="gray.200"
+          spacing="0px"
           w="full"
           divider={<StackDivider borderColor="gray.300" />}>
-          <Box w="full" px="4" py="2">
+          <Box bg="blue.50" w="full" px="4" py="2">
             <Text my="1" isTruncated fontSize="md" color="gray.800">
               CoffeeCoffee to testCoffee to test to test
             </Text>
@@ -47,7 +48,7 @@ const leftPanel = () => {
               the future
             </Text>
           </Box>
-          <Box w="full" px="4" py="2">
+          <Box bg="white" w="full" px="4" py="2">
             <Text fontSize="md" color="gray.800">
               Coffee to test
             </Text>
@@ -55,7 +56,7 @@ const leftPanel = () => {
               Grounds I to test in the future
             </Text>
           </Box>
-          <Box w="full" px="4" py="2" pb="4">
+          <Box bg="white" w="full" px="4" py="2" pb="4">
             <Text fontSize="md" color="gray.800">
               Coffee to test
             </Text>
