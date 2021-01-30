c6f82ad5139c829fe7b3964e48b48a44ef196710
romanmunar <roman.munar1@gmail.com>
2021-01-30 19:22:57 +0800

    added shop and ratings asyncThunks

--- src/slices/selectedItems/selectedItemsSlice.ts
+++ src/slices/selectedItems/selectedItemsSlice.ts
@@ -1,15 +1,33 @@
 import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "src/App/rootReducer";
-import { getItem } from "src/lib/api";
-import { ItemDetailed, SearchItem } from "src/lib/types";
+import { getItem, getShopBrief, getRatings } from "src/lib/api";
+import { ItemDetailed, Rating, RatingQuery, SearchItem, ShopBrief } from "src/lib/types";
 
 export interface SelectedItem extends SearchItem {
   error: ErrorValues | null | boolean;
   fetchStatus: "fulfilled" | "pending" | "idle";
+  type: "brief";
+  shop: Partial<SelectedItemShop>;
+  ratings: Partial<SelectedItemRatings>;
 }
+
+export interface SelectedItemShop extends ShopBrief {
+  fetchStatus: "fulfilled" | "pending" | "idle";
+  error: ErrorValues | null | boolean;
+}
+
+export interface SelectedItemRatings {
+  ratings: Rating[];
+  fetchStatus: "fulfilled" | "pending" | "idle";
+  error: ErrorValues | null | boolean;
+}
+
 export interface SelectedItemDetailed extends ItemDetailed {
   error: ErrorValues | null | boolean;
   fetchStatus: "fulfilled" | "pending" | "idle";
+  type: "detailed";
+  shop: SelectedItemShop;
+  ratings: SelectedItemRatings;
 }
 
 interface SelectedItemsState {
@@ -28,22 +46,17 @@ interface ErrorValues {
   shopid: number;
 }
 
-export const fetchDetailedItem = createAsyncThunk<
-  ItemDetailed,
-  {
-    itemid: number;
-    shopid: number;
-  },
+export const fetchShopBrief = createAsyncThunk<
+  { shop: ShopBrief; itemid: number },
+  SelectedItem | SelectedItemDetailed,
   { rejectValue: ErrorValues; state: RootState }
 >(
-  "selectedItems/fetchDetailedById",
-  async (data, { rejectWithValue, dispatch }) => {
-    const { itemid, shopid } = data;
-
-    dispatch(selectedItemStart({ itemid }));
-
+  "selectedItems/fetchShopBriefById",
+  async ({ itemid, shopid }, { rejectWithValue }) => {
     try {
-      return await getItem({ itemid, shopid });
+      const shop = await getShopBrief({ itemid, shopid });
+
+      return { shop, itemid, shopid };
     } catch (err) {
       return rejectWithValue({ message: err.message, itemid, shopid });
     }
@@ -52,6 +65,77 @@ export const fetchDetailedItem = createAsyncThunk<
     condition: ({ itemid }, { getState }) => {
       const { selectedItems } = getState().selectedItemsReducer;
       const item = selectedItems.find((i) => i.itemid === itemid);
+      if (!item || item?.type !== "detailed") return false;
+
+      if (item.shop.fetchStatus == "fulfilled" || item.shop.fetchStatus == "pending") {
+        return false;
+      }
+    },
+  }
+);
+
+export const fetchItemRatings = createAsyncThunk<
+  { ratings: Rating[]; itemid: number },
+  RatingQuery,
+  { rejectValue: ErrorValues; state: RootState }
+>(
+  "selectedItems/fetchItemRatingsById",
+  async (ratingQuery, { rejectWithValue }) => {
+    const { itemid, shopid } = ratingQuery;
+
+    try {
+      const ratings = await getRatings(ratingQuery);
+      return { ratings, itemid, shopid };
+    } catch (err) {
+      return rejectWithValue({
+        message: err.message,
+        itemid: ratingQuery.itemid,
+        shopid: ratingQuery.shopid,
+      });
+    }
+  },
+  {
+    condition: ({ itemid }, { getState }) => {
+      const { selectedItems } = getState().selectedItemsReducer;
+      const item = selectedItems.find((i) => i.itemid === itemid);
+      if (!item || item?.type !== "detailed") return false;
+
+      if (item.ratings.fetchStatus == "pending") {
+        return false;
+      }
+    },
+  }
+);
+
+export const fetchDetailedItem = createAsyncThunk<
+  SelectedItemDetailed,
+  SelectedItem | SelectedItemDetailed,
+  { rejectValue: ErrorValues; state: RootState }
+>(
+  "selectedItems/fetchDetailedById",
+  async (data, { rejectWithValue }) => {
+    const { itemid, shopid } = data;
+
+    try {
+      const item = await getItem({ itemid, shopid });
+      const newItem = {
+        ...item,
+        ...data,
+        fetchStatus: "fulfilled",
+        error: null,
+        type: "detailed",
+      } as SelectedItemDetailed;
+      return newItem;
+    } catch (err) {
+      return rejectWithValue({ message: err.message, itemid, shopid });
+    }
+  },
+  {
+    condition: ({ itemid }, { getState }) => {
+      const { selectedItems } = getState().selectedItemsReducer;
+      const item = selectedItems.find((i) => i.itemid === itemid);
+      if (!item || item?.type !== "brief") return false;
+
       if (item?.fetchStatus == "fulfilled" || item?.fetchStatus == "pending") {
         // Already fetched or in progress, don't need to re-fetch
         return false;
@@ -68,31 +152,59 @@ export const selectedItemsShopee = createSlice({
       if (state.isEmpty) state.isEmpty = false;
 
       const { item } = action.payload;
-      const newItem: SelectedItem = { ...item, fetchStatus: "idle", error: null };
+      const newItem: SelectedItem = {
+        ...item,
+        fetchStatus: "idle",
+        error: null,
+        type: "brief",
+        shop: { fetchStatus: "idle", error: null },
+        ratings: { ratings: [], fetchStatus: "idle", error: null },
+      };
 
       state.selectedItems.push(newItem);
     },
-    unselectItem(state, action: PayloadAction<{ selectedItem: SelectedItem | SearchItem }>) {
+    unselectItem(
+      state,
+      action: PayloadAction<{ selectedItem: any & { itemid: number; shopid: number } }>
+    ) {
       if (state.selectedItems.length <= 1) state.isEmpty = true;
 
       const { selectedItem } = action.payload;
       state.selectedItems = state.selectedItems.filter((i) => selectedItem.itemid !== i.itemid);
     },
-    selectedItemStart(state, action: PayloadAction<{ itemid: number }>) {
+    swapFirstAndSecondItems(state) {
+      const [firstEl, secondEl, ...rest] = state.selectedItems;
+      state.selectedItems = [secondEl, firstEl, ...rest];
+    },
+    fetchDetailedItemStart(state, action: PayloadAction<{ itemid: number }>) {
       const item = state.selectedItems.find((i) => i.itemid === action.payload.itemid);
       if (!item) return;
       item.fetchStatus = "pending";
     },
+    fetchItemRatingsStart(state, action: PayloadAction<{ itemid: number }>) {
+      const item = state.selectedItems.find((i) => i.itemid === action.payload.itemid);
+      if (!item) return;
+      item.ratings.fetchStatus = "pending";
+    },
   },
   extraReducers: (builder) => {
+    builder.addCase(fetchDetailedItem.pending, (state, { meta: { arg } }) => {
+      const item = state.selectedItems.find((i) => i.itemid === arg.itemid);
+      if (!item) return;
+      item.fetchStatus = "pending";
+    });
     builder.addCase(fetchDetailedItem.fulfilled, (state, { payload: item }) => {
-      //remove past
-      state.selectedItems.filter((i) => i.itemid === item.itemid);
-      const newItem = { ...item, fetchStatus: "idle", error: null } as SelectedItemDetailed;
-      state.selectedItems.push(newItem);
+      const { itemid } = item;
+      const itemIndex = state.selectedItems.findIndex((i) => i.itemid === itemid);
+
+      if (itemIndex === -1) {
+        return;
+      } else {
+        state.selectedItems.splice(itemIndex, 1, item);
+      }
     });
     builder.addCase(fetchDetailedItem.rejected, (state, { payload }) => {
-      const item = state.selectedItems.find((i) => i.itemid !== payload?.itemid);
+      const item = state.selectedItems.find((i) => i.itemid === payload?.itemid);
       if (!item) {
         return;
       }
@@ -102,8 +214,85 @@ export const selectedItemsShopee = createSlice({
       } else {
         item.error = true;
       }
+      item.fetchStatus = "idle";
+    });
+    builder.addCase(fetchShopBrief.fulfilled, (state, { payload: shopRes }) => {
+      const { shop, itemid } = shopRes;
+      const itemIndex = state.selectedItems.findIndex((i) => i.itemid === itemid);
+
+      if (itemIndex === -1) {
+        return;
+      } else {
+        const oldItem = state.selectedItems[itemIndex] as SelectedItemDetailed;
+        if (oldItem.shop.fetchStatus === "fulfilled") return;
+
+        const newShop: SelectedItemShop = { ...shop, error: false, fetchStatus: "fulfilled" };
+
+        const newItem = {
+          ...oldItem,
+          shop: newShop,
+        } as SelectedItemDetailed;
+        state.selectedItems.splice(itemIndex, 1, newItem);
+      }
+    });
+    builder.addCase(fetchShopBrief.pending, (state, { meta: { arg } }) => {
+      const item = state.selectedItems.find((i) => i.itemid === arg.itemid);
+      if (!item) return;
+      item.shop.fetchStatus = "pending";
+    });
+    builder.addCase(fetchShopBrief.rejected, (state, { payload }) => {
+      const item = state.selectedItems.find((i) => i.itemid === payload?.itemid);
+      if (!item) {
+        return;
+      }
+
+      if (payload) {
+        item.error = payload;
+      } else {
+        item.error = true;
+      }
+      item.fetchStatus = "idle";
+    });
+    builder.addCase(fetchItemRatings.pending, (state, { meta: { arg } }) => {
+      const item = state.selectedItems.find((i) => i.itemid === arg.itemid);
+      if (!item) return;
+      item.ratings.fetchStatus = "pending";
+    });
+    builder.addCase(fetchItemRatings.fulfilled, (state, { payload: item }) => {
+      const { ratings, itemid } = item;
+      const itemIndex = state.selectedItems.findIndex((i) => i.itemid === itemid);
+
+      if (itemIndex === -1) {
+        return;
+      } else {
+        const oldItem = state.selectedItems[itemIndex] as SelectedItemDetailed;
+        const newRatings: SelectedItemRatings = {
+          ratings: [...oldItem.ratings.ratings, ...ratings],
+          error: false,
+          fetchStatus: "fulfilled",
+        };
+
+        const newItem = {
+          ...oldItem,
+          ratings: newRatings,
+        } as SelectedItemDetailed;
+        state.selectedItems.splice(itemIndex, 1, newItem);
+      }
+    });
+    builder.addCase(fetchItemRatings.rejected, (state, { payload }) => {
+      const item = state.selectedItems.find((i) => i.itemid === payload?.itemid);
+      if (!item) {
+        return;
+      }
+
+      if (payload) {
+        item.error = payload;
+      } else {
+        item.error = true;
+      }
+      item.fetchStatus = "idle";
     });
   },
 });
-export const { selectItem, unselectItem, selectedItemStart } = selectedItemsShopee.actions;
+export const { selectItem, unselectItem, swapFirstAndSecondItems } = selectedItemsShopee.actions;
 export default selectedItemsShopee.reducer;
