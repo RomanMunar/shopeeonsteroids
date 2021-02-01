8affa980c64b967c57c43cfdc59ccf8a307292a0
romanmunar <roman.munar1@gmail.com>
2021-02-01 23:37:05 +0800

    removed unused modal

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,6 @@
-import { ChakraProvider, Modal, ModalContent, ModalOverlay } from "@chakra-ui/react";
-import { useDispatch, useSelector } from "react-redux";
+import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter, Routes } from "react-router-dom";
-import { ItemDisplayView, Layout } from "src/components";
-import { closeModal } from "src/slices/ui/UISlice";
-import { RootState } from "./rootReducer";
-import { AnimatedRoute } from "src/components";
+import { AnimatedRoute, Layout } from "src/components";
 import { Bookmarks } from "./Bookmarks";
 import { Main } from "./Main";
 import { Search } from "./Search";
@@ -19,19 +15,8 @@ const routes = [
 ];
 
 function App() {
-  const { displayModal, modalView } = useSelector((state: RootState) => state.UIReducer);
-  const dispatch = useDispatch();
-
   return (
     <ChakraProvider resetCSS>
-      <Modal isOpen={displayModal} onClose={() => dispatch(closeModal())}>
-        <ModalOverlay />
-        <ModalContent>
-          {modalView === "itemDisplay" && (
-            <ItemDisplayView onClose={() => dispatch(closeModal())} />
-          )}
-        </ModalContent>
-      </Modal>
       <BrowserRouter>
         <Layout>
           <Routes>
