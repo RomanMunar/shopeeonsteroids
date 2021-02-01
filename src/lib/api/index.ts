8e69232949bc336eddfe4a6f8c4a41ab9214e1f9
romanmunar <roman.munar1@gmail.com>
2021-02-01 23:39:44 +0800

    fixed endpoint and added error handling

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,47 +1,65 @@
 import queryString from "query-string";
-import { Item, ItemDetailed, Rating, RatingQuery, SearchQuery, ShopDetailed } from "../types";
+import { filterByUniqueField } from "src/lib/utils";
+import { RatingQuery, SearchQuery } from "../types";
+import {
+  SearchResponse,
+  ShopeeItemDetailResponse,
+  ShopeeRatingResponse,
+  ShopeeShopBriefResponse,
+  ShopeeShopDetailResponse,
+} from "../types/Api";
 
 export const searchAPI = async (query: SearchQuery) => {
   const stringQuery = queryString.stringify(query);
   const response = await fetch(`/api/v2/search/?${stringQuery}`);
 
-  const items: Item[] = await response.json();
+  const { items, error } = (await response.json()) as SearchResponse;
+  if (error) {
+    throw new Error("Error on searchApi");
+  }
 
-  return items;
+  return filterByUniqueField(items, "itemid");
 };
 
 export const getItem = async ({ itemid, shopid }: { itemid: number; shopid: number }) => {
   const stringQuery = queryString.stringify({ itemid, shopid });
-
   const response = await fetch(`/api/v2/item/get/?${stringQuery}`);
-  const items: ItemDetailed = await response.json();
 
-  return items;
+  const { item, error } = (await response.json()) as ShopeeItemDetailResponse;
+  if (error) {
+    throw new Error("Error on searchApi");
+  }
+
+  return item;
 };
 
-export const getItemRating = async (ratingQuery: RatingQuery) => {
+export const getRatings = async (ratingQuery: RatingQuery) => {
   const stringQuery = queryString.stringify(ratingQuery);
 
   const response = await fetch(`/api/v2/item/get_rating/?${stringQuery}`);
-  const items: Rating[] = await response.json();
-
-  return items;
+  const { data, error } = (await response.json()) as ShopeeRatingResponse;
+  if (error) {
+    throw new Error("Error on searchApi");
+  }
+  return data.ratings;
 };
 
-export const getShopBrief = async (ratingQuery: RatingQuery) => {
-  const stringQuery = queryString.stringify(ratingQuery);
+export const getShopBrief = async ({ itemid, shopid }: { itemid: number; shopid: number }) => {
+  const stringQuery = queryString.stringify({ itemid, shopid });
 
-  const response = await fetch(`/api/v2/shop/?${stringQuery}`);
-  const items: Rating[] = await response.json();
-
-  return items;
+  const response = await fetch(`/api/v2/shop/get/?${stringQuery}`);
+  const { data, error } = (await response.json()) as ShopeeShopBriefResponse;
+  if (error) {
+    throw new Error("Error on searchApi");
+  }
+  return data;
 };
 
 export const getShop = async ({ shopid }: { shopid: number }) => {
   const stringQuery = queryString.stringify({ shopid });
 
   const response = await fetch(`/api/v4/shop/get_shop_detail/?${stringQuery}`);
-  const items: ShopDetailed = await response.json();
+  const { data } = (await response.json()) as ShopeeShopDetailResponse;
 
-  return items;
+  return data;
 };
