442b7a10115882fe6fe9bd5e5cf419ef103f3300
romanmunar <roman.munar1@gmail.com>
2021-02-02 21:05:28 +0800

    flattened settings

--- src/lib/data/defaultSearchSettings.ts
+++ src/lib/data/defaultSearchSettings.ts
@@ -2,33 +2,21 @@ import { CompareLayout } from "src/slices/ui/UISlice";
 import { SellerLocation, SearchSort } from "../types";
 
 export interface Settings {
-  ui: {
-    filterPanelCollapsed: boolean;
-    compareLayout: CompareLayout;
-  };
-  search: {
-    sellerLocation: SellerLocation[];
-    searchSort: SearchSort;
-    itemRatingOnly: 2 | 3 | 4 | 5;
-    shopeeVerifiedOnly: boolean;
-  };
-  seller: {
-    lastActive: number;
-  };
+  filterPanelCollapsed: boolean;
+  compareLayout: CompareLayout;
+  sellerLocation: SellerLocation[];
+  searchSort: SearchSort;
+  itemRatingOnly: 2 | 3 | 4 | 5;
+  shopeeVerifiedOnly: boolean;
+  lastActive: number;
 }
 
 export const defaultSettings: Settings = {
-  ui: {
-    filterPanelCollapsed: false,
-    compareLayout: "double",
-  },
-  search: {
-    searchSort: "sales",
-    sellerLocation: ["Metro Manila", "Mindanao", "North Luzon", "South Luzon", "Visayas"],
-    itemRatingOnly: 4,
-    shopeeVerifiedOnly: false,
-  },
-  seller: {
-    lastActive: 3,
-  },
+  filterPanelCollapsed: false,
+  compareLayout: "double",
+  searchSort: "sales",
+  sellerLocation: ["Metro Manila"],
+  itemRatingOnly: 4,
+  shopeeVerifiedOnly: false,
+  lastActive: 3,
 };
