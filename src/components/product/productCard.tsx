60e82ecbe738b825d144433d8d1f5da4967cb9e4
romanmunar <roman.munar1@gmail.com>
2021-02-02 21:00:57 +0800

    added icons on product card

--- src/components/product/productCard.tsx
+++ src/components/product/productCard.tsx
@@ -14,21 +14,7 @@ interface Props {
 }
 
 const productCard = ({ item, addToSelectedItems, removeToSelectedItems, selected }: Props) => {
-  const [mounted, setMounted] = useState(false);
   const [hovered, setHovered] = useState(false);
-  const [renderImage, setRenderImage] = useState(false);
-  const intersectionRef = useRef(null);
-  const intersection = useIntersection(intersectionRef);
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-  useEffect(() => {
-    if (!mounted) return;
-    if (!intersection) return;
-    if (intersection.intersectionRatio >= 1) {
-      setRenderImage(true);
-    }
-  }, [intersection]);
 
   const onClick = () => {
     selected ? removeToSelectedItems(item) : addToSelectedItems(item);
@@ -51,20 +37,18 @@ const productCard = ({ item, addToSelectedItems, removeToSelectedItems, selected
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
       layoutId={`card-container-${item.itemid}`}>
-      <Box ref={intersectionRef} position="relative" width="100%" paddingTop="100%">
-        <LazyBox load={renderImage}>
-          <Box
-            as="img"
-            position="absolute"
-            top="0"
-            left="0"
-            width="100%"
-            height="100%"
-            bg="#fff"
-            src={`https://cf.shopee.ph/file/${item.image}`}
-            alt=""
-          />
-        </LazyBox>
+      <Box position="relative" width="100%" paddingTop="100%">
+        <Box
+          as="img"
+          position="absolute"
+          top="0"
+          left="0"
+          width="100%"
+          height="100%"
+          bg="#fff"
+          src={`https://cf.shopee.ph/file/${item.image}`}
+          alt=""
+        />
       </Box>
       <Box p="2">
         <Text fontSize="sm" lineHeight="1rem" noOfLines={2}>
@@ -76,16 +60,39 @@ const productCard = ({ item, addToSelectedItems, removeToSelectedItems, selected
               ₱{item.price / 100000}
             </Text>
             <Text fontSize="sm" lineHeight="1rem" noOfLines={2}>
-              {kFormat(item.sold)}
+              {kFormat(item.sold)}{" "}
+              <Box as="span" fontSize="xs">
+                sold/mon
+              </Box>
             </Text>
           </Flex>
           <Flex my="1" alignItems="center" justifyContent="space-between">
-            <Text fontSize="sm" lineHeight="1rem" noOfLines={2}>
-              {item.item_rating.rating_star.toFixed(2)}
-            </Text>
-            <Text fontSize="sm" lineHeight="1rem" noOfLines={2}>
-              {item.liked_count}
-            </Text>
+            <Flex>
+              <Text fontSize="sm" lineHeight="1rem" noOfLines={2}>
+                {item.item_rating.rating_star.toFixed(2)}
+              </Text>{" "}
+              <svg
+                width="16px"
+                height="16px"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor">
+                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+              </svg>
+            </Flex>
+            <Flex alignItems="center">
+              <Text fontSize="sm" lineHeight="1rem" noOfLines={2}>
+                {item.liked_count}
+              </Text>{" "}
+              <svg
+                width="14px"
+                height="14px"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor">
+                <path d="M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z" />
+              </svg>
+            </Flex>
           </Flex>
         </Box>
       </Box>
