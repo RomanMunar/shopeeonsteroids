d5df93780db466dcf91752407a14f1c23b53d000
romanmunar <roman.munar1@gmail.com>
2021-02-02 20:58:32 +0800

    added guides panel

---
+++ src/App/Main/GuidesPanel/guidesPanel.tsx
@@ -0,0 +1,67 @@
+import { Guide } from "src/lib/types";
+import {
+  Flex,
+  Heading,
+  Box,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  StackDivider,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { Search } from "src/components/icons";
+import { GuideItemCard } from "src/components/product";
+
+interface Props {
+  guides: Guide[];
+  displayedGuide: Guide;
+  previewGuide: (item: Guide) => void;
+}
+
+const guidesPanel = ({ guides, displayedGuide, previewGuide }: Props) => {
+  return (
+    <Flex overflowY="hidden" h="100vh" flexDirection="row">
+      <Flex
+        flexDirection="column"
+        overflowY="auto"
+        h="100vh"
+        w="300px"
+        borderRight="1px"
+        borderColor="gray.300"
+        flex="none">
+        <Heading as="h3" size="sm" px="2" py="4" alignSelf="start">
+          ONBOARDING GUIDES 🎉🎉
+        </Heading>
+        <Box my="4" px="2">
+          <InputGroup>
+            <InputLeftElement pointerEvents="none">
+              <Search width="20" height="20" />
+            </InputLeftElement>
+            <Input type="text" placeholder="Coffee..." />
+          </InputGroup>
+        </Box>
+        <Box bg="gray.50" display="flex" alignItems="start" flex="1" shadow="inner">
+          <VStack
+            borderY="1px"
+            borderColor="gray.200"
+            bg="gray.50"
+            spacing="0px"
+            w="full"
+            divider={<StackDivider borderColor="gray.300" />}>
+            {guides.map((g) => (
+              <GuideItemCard
+                key={g.id}
+                guide={g}
+                active={displayedGuide.id === g.id}
+                previewGuide={previewGuide}
+              />
+            ))}
+          </VStack>
+        </Box>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default guidesPanel;
---
+++ src/App/Main/GuidesPanel/index.ts
@@ -0,0 +1,1 @@
+export { default as GuidesPanel } from "./guidesPanel";
