447bb95b8af97dbcd6c6445e187df8d3ac36b622
romanmunar <roman.munar1@gmail.com>
2021-02-03 19:40:09 +0800

    added collapse

--- src/App/Bookmarks/BookmarksPanel/bookmarksPanel.tsx
+++ src/App/Bookmarks/BookmarksPanel/bookmarksPanel.tsx
@@ -6,9 +6,12 @@ import {
   InputGroup,
   InputLeftElement,
   StackDivider,
+  useMediaQuery,
   VStack,
 } from "@chakra-ui/react";
-import { Search } from "src/components/icons";
+import { useState } from "react";
+import { MotionBox } from "src/components";
+import { ArrowRight, Search } from "src/components/icons";
 import { BookmarkItemCard } from "src/components/product";
 import { BookmarkItem } from "src/lib/types";
 
@@ -29,48 +32,80 @@ const leftPanel = ({
   updateBookmarkTitle,
   removeBookmarkItem,
 }: Props) => {
+  const isMobile = useMediaQuery("(max-width: 500px)")[0];
+  const [collapsed, setCollapse] = useState(isMobile);
+  const toggleCollapse = () => setCollapse((p) => !p);
   return (
-    <Flex
-      flexDirection="column"
+    <MotionBox
+      animate={{
+        overflowY: collapsed ? "hidden" : "auto",
+        width: collapsed ? 0 : "300px",
+        height: isMobile ? (collapsed ? "50px" : "100vh") : "100vh",
+        bottom: isMobile ? (collapsed ? 50 : 0) : 0,
+      }}
+      left={0}
+      p={collapsed ? 5 : 0}
+      bg="white"
+      position={["absolute", "relative"]}
+      zIndex="90"
       overflowY="auto"
-      h="100vh"
-      w="300px"
-      borderRight="1px"
+      borderRight={isMobile ? (collapsed ? 0 : "1px") : "1px"}
       borderColor="gray.300"
       flex="none">
-      <Heading as="h3" size="sm" px="2" py="4" alignSelf="start">
-        BOOKMARKS
-      </Heading>
-      <Box my="4" px="2">
-        <InputGroup>
-          <InputLeftElement pointerEvents="none">
-            <Search width="20" height="20" />
-          </InputLeftElement>
-          <Input type="text" placeholder="Coffee..." />
-        </InputGroup>
+      <Box display={collapsed ? "none" : "flex"} flexDirection="column">
+        <Heading as="h3" size="sm" px="2" py="4" alignSelf="start">
+          BOOKMARKS
+        </Heading>
+        <Box my="4" px="2">
+          <InputGroup>
+            <InputLeftElement pointerEvents="none">
+              <Search width="20" height="20" />
+            </InputLeftElement>
+            <Input type="text" placeholder="Coffee..." />
+          </InputGroup>
+        </Box>
+        <Box bg="gray.50" display="flex" alignItems="start" flex="1" shadow="inner">
+          <VStack
+            borderY="1px"
+            borderColor="gray.200"
+            bg="gray.50"
+            spacing="0px"
+            w="full"
+            divider={<StackDivider borderColor="gray.300" />}>
+            {bookmarks.map((i) => (
+              <BookmarkItemCard
+                key={i.id}
+                updateBookmarkDescription={updateBookmarkDescription}
+                updateBookmarkTitle={updateBookmarkTitle}
+                removeBookmarkItem={removeBookmarkItem}
+                active={i.id === displayedBookmark.id}
+                item={i}
+                previewBookmark={previewBookmark}
+              />
+            ))}
+          </VStack>
+        </Box>
       </Box>
-      <Box bg="gray.50" display="flex" alignItems="start" flex="1" shadow="inner">
-        <VStack
-          borderY="1px"
-          borderColor="gray.200"
-          bg="gray.50"
-          spacing="0px"
-          w="full"
-          divider={<StackDivider borderColor="gray.300" />}>
-          {bookmarks.map((i) => (
-            <BookmarkItemCard
-              key={i.id}
-              updateBookmarkDescription={updateBookmarkDescription}
-              updateBookmarkTitle={updateBookmarkTitle}
-              removeBookmarkItem={removeBookmarkItem}
-              active={i.id === displayedBookmark.id}
-              item={i}
-              previewBookmark={previewBookmark}
-            />
-          ))}
-        </VStack>
-      </Box>
-    </Flex>
+      <MotionBox
+        animate={{ rotate: collapsed ? -180 : 0 }}
+        border={isMobile ? "2px" : "0"}
+        borderColor={isMobile ? "gray.300" : "0"}
+        position="absolute"
+        top="8px"
+        right="8px"
+        as="button"
+        onClick={toggleCollapse}
+        alignItems="center"
+        justifyContent="center"
+        display="flex"
+        rounded="md"
+        minW="30px"
+        minH="30px"
+        maxW="30px"
+        maxH="30px">
+        <ArrowRight width="20px" height="20px" />
+      </MotionBox>
+    </MotionBox>
   );
 };
 
--- src/App/Main/GuidesPanel/guidesPanel.tsx
+++ src/App/Main/GuidesPanel/guidesPanel.tsx
@@ -7,8 +7,11 @@ import {
   InputLeftElement,
   StackDivider,
   VStack,
+  useMediaQuery,
 } from "@chakra-ui/react";
-import { Search } from "src/components/icons";
+import { useState } from "react";
+import { MotionBox } from "src/components";
+import { ArrowRight, Search } from "src/components/icons";
 import { GuideItemCard } from "src/components/product";
 import { Guide } from "src/lib/types";
 
@@ -19,8 +22,28 @@ interface Props {
 }
 
 const guidesPanel = ({ guides, displayedGuide, previewGuide }: Props) => {
+  const [collapsed, setCollapse] = useState(false);
+  const toggleCollapse = () => setCollapse((p) => !p);
+  const isMobile = useMediaQuery("(max-width: 500px)")[0];
+
   return (
-    <Flex overflowY="hidden" h="100vh" flexDirection="row">
+    <MotionBox
+      animate={{
+        overflowY: collapsed ? "hidden" : "auto",
+        width: collapsed ? 0 : "300px",
+        height: isMobile ? (collapsed ? "50px" : "100vh") : "100vh",
+        bottom: isMobile ? (collapsed ? 50 : 0) : 0,
+      }}
+      p={collapsed ? 5 : 0}
+      bg="white"
+      position={["absolute", "relative"]}
+      left={0}
+      zIndex="90"
+      borderRight={isMobile ? (collapsed ? 0 : "1px") : "1px"}
+      borderColor="gray.300"
+      overflowY="hidden"
+      h="100vh"
+      flexDirection="row">
       <Flex
         flexDirection="column"
         overflowY="auto"
@@ -59,7 +82,26 @@ const guidesPanel = ({ guides, displayedGuide, previewGuide }: Props) => {
           </VStack>
         </Box>
       </Flex>
-    </Flex>
+      <MotionBox
+        animate={{ rotate: collapsed ? -180 : 0 }}
+        border={isMobile ? "2px" : "0"}
+        borderColor={isMobile ? "gray.300" : "0"}
+        position="absolute"
+        top="8px"
+        right="8px"
+        as="button"
+        onClick={toggleCollapse}
+        alignItems="center"
+        justifyContent="center"
+        display="flex"
+        rounded="md"
+        minW="30px"
+        minH="30px"
+        maxW="30px"
+        maxH="30px">
+        <ArrowRight width="20px" height="20px" />
+      </MotionBox>
+    </MotionBox>
   );
 };
 
